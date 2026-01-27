aefa69b34c09ca19ff81161c19157bd0bb5f3caa
Jackzzs <jackzzs@outlook.com>
2026-01-27 16:18:52 +0800

    Bump version: 1.0.16 → 1.0.17

--- _bindings/python/setup.py
+++ _bindings/python/setup.py
@@ -618,7 +618,7 @@ ensure_placeholder_masktunnellib()
 
 setup(
     name="masktunnel",
-    version="1.0.16",
+    version="1.0.17",
     description="Python bindings for MaskTunnel (Go)",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",

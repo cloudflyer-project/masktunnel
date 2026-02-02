e34b6f04bb5dc9bdd00bec67023339cc8ffaba7a
zetxtech <i@zetx.tech>
2026-02-02 18:22:04 +0800

    Bump version: 1.0.20 → 1.0.21

--- _bindings/python/masktunnel/__init__.py
+++ _bindings/python/masktunnel/__init__.py
@@ -3,7 +3,7 @@
 This package provides a Python-friendly API on top of the generated native bindings.
 """
 
-__version__ = "1.0.20"
+__version__ = "1.0.21"
 
 from ._server import Server, ServerOptions
 from ._utils import set_log_level
